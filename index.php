154558ceb1ad481ab773869995aed709ce6c2cad
Ty Mick <ty@tymick.me>
2020-04-26 18:18:24 -0400

    Adjust database location

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 // Connect to the database
 try {
-  $pdo = new PDO("sqlite:db/todos.db");
+  $pdo = new PDO("sqlite:todos.db");
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+  // Create table if necessary
+  $pdo->query(
+    "CREATE TABLE IF NOT EXISTS Todos (id INTEGER PRIMARY KEY, Task TEXT, Complete INTEGER, Created INTEGER, Completed INTEGER)"
+  );
 } catch (PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
